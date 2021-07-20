f83bef50dd6dc913786863550cc7670d42c5320c
oen0326 <hiyeahin@gmail.com>
2021-07-20 13:37:41 +0900

    plus

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,69 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var sum = 0
+        var current = ""
+
+        ca.setOnClickListener {
+            sum = 0
+            current = ""
+            result.setText(sum.toString())
+        }
+
+        plus.setOnClickListener {
+            sum += current.toInt()
+            result.setText(sum.toString())
+            current = ""
+        }
+
+        one.setOnClickListener {
+            current += "1"
+            result.setText(current)
+        }
+
+        two.setOnClickListener {
+            current += "2"
+            result.setText(current)
+        }
+        three.setOnClickListener {
+            current += "3"
+            result.setText(current)
+        }
+        four.setOnClickListener {
+            current += "4"
+            result.setText(current)
+        }
+        five.setOnClickListener {
+            current += "5"
+            result.setText(current)
+        }
+        six.setOnClickListener {
+            current += "6"
+            result.setText(current)
+        }
+        seven.setOnClickListener {
+            current += "7"
+            result.setText(current)
+        }
+        eight.setOnClickListener {
+            current += "8"
+            result.setText(current)
+        }
+        nine.setOnClickListener {
+            current += "9"
+            result.setText(current)
+        }
+        zero.setOnClickListener {
+            current += "0"
+            result.setText(current)
+        }
     }
 }
